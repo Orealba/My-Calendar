6cc9302cc7eeb2a11fdf8390f8c88383525a27a8
Orealba Soriano <orealbas@gmail.com>
2022-11-25 18:03:01 +0100

    image change it

--- src/Components/CalendarHeader.js
+++ src/Components/CalendarHeader.js
@@ -11,7 +11,7 @@ const CalendarHeader = () => {
     <div className="row calendarHeader__box">
       <div className="col-md-4">
         <img
-          src="Pictures/FactorialLogo.png"
+          src="/Pictures/FactorialLogo.png"
           className="calendar-header-logo"
           onClick={redirection}
         ></img>
